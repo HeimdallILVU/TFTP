df0a4421263871f1ffe91cd1ef1f3198948cfa6b
heimdallILVU <jeremyjeremy552@gmail.com>
2023-12-21 08:57:10 +0100

    tmp

--- include/libfun.h
+++ include/libfun.h
@@ -1,14 +1,29 @@
+#define DEBUG 1
+
+#define WRITE_ERROR "Error writing to file descriptor"
 #define NOT_ENOUGH_ARGS "Not enough args"
 #define TOO_MANY_ARGS "Too many args"
-#define GET_ADDR_INFO_ERROR "Erreur lors de la résolution de l'adresse"
+#define GET_ADDR_INFO_ERROR "Error during address resolution"
 #define SOCKET_CONNECT_ERROR "Error connecting"
 #define SOCKET_CREATE_ERROR "Error Creating socket"
 #define IP_INVALID_ERROR "Invalid IP address"
-
+#define ERROR_SENDING_PACKET "Error sending packet"
+#define MESSAGE_SUCCESS_READ_REQUEST "Read request packet sent successfully.\n"
+#define MESSAGE_SUCCESS_WRITE_REQUEST "Write request packet sent successfully.\n"
+#define MESSAGE_SUCCESS_DATA "Data packet sent successfully.\n"
+#define MESSAGE_SUCCESS_ACK "Ack packet sent successfully.\n"
+#define READ_REQUEST "\x00\x01heimdall.txt\x00netascii\x00"
 #define PORT "69"
+
+#define TFTP_OPCODE_RRQ_READ 1
+#define TFTP_OPCODE_RRQ_WRITE 2
+#define TFTP_OPCODE_DATA 3
+#define TFTP_OPCODE_ACK 4
+#define TFTP_BLOCK_SIZE 512
+#define TFTP_MODE "netascii"
 
 void print_message(char * message);
 void print_error(char * error);
 
 void gettftp(char * host, char * file);
-void puttftp(char * host, char * file);
+void puttftp(char * host, char * file); 
--- src/libfun.c
+++ src/libfun.c
@@ -44,7 +44,7 @@ char * get_ip(char * host) { // Get ip from host url
 
     // 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; // IPv4 or IPv6
+    hints.ai_family = AF_INET; // IPv4
     hints.ai_socktype = SOCK_DGRAM; // Datagrams
 
     // 
@@ -79,7 +79,7 @@ char * get_ip(char * host) { // Get ip from host url
     return NULL;
 }
 
-void get_socket(char * ip) { // Get socket from ip
+int get_socket(char * ip) { // Get socket from ip
     int sockfd;
     struct sockaddr_in server_addr;
 
@@ -104,10 +104,205 @@ void get_socket(char * ip) { // Get socket from ip
         exit(EXIT_FAILURE);
     }
 
-    print_message("Connected\n");
+    return sockfd;
+
+}
+
+void print_raw_byte(char * packet, int packetSize) {
+    // Print the rrqPacket as hexadecimal values
+    for (int i = 0; i < packetSize; ++i) {
+        char message[512];
+        sprintf(message, "%02X ", packet[i]);
+        print_message(message);
+    }
+
+    printf("\n");
+}
 
 
-    close(sockfd);
+
+void sendTFTPReadRequest(const char *filename, int sockfd) {
+    // Prepare the RRQ packet
+    char rrqPacket[516]; // Max size for a TFTP packet
+    memset(rrqPacket, 0, sizeof(rrqPacket));
+
+    // Opcode (Read Request)
+    rrqPacket[0] = (TFTP_OPCODE_RRQ_READ >> 8) & 0xFF;
+    rrqPacket[1] = TFTP_OPCODE_RRQ_READ & 0xFF;
+
+    // Filename
+    strcpy(&rrqPacket[2], filename);
+
+    // Null terminator after the filename
+    int filenameLength = strlen(filename);
+    rrqPacket[2 + filenameLength] = '\0';
+
+    // Mode
+    strcpy(&rrqPacket[2 + filenameLength + 1], TFTP_MODE);
+
+    // Null terminator after the mode
+    int modeLength = strlen(TFTP_MODE);
+    rrqPacket[2 + filenameLength + 1 + modeLength] = '\0';
+
+    //print_raw_byte(rrqPacket, sizeof(rrqPacket));
+
+    // Send the RRQ packet
+    ssize_t bytesSent = send(sockfd, rrqPacket, sizeof(rrqPacket), 0);
+    if (bytesSent == -1) {
+        print_error(ERROR_SENDING_PACKET);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (DEBUG) print_message(MESSAGE_SUCCESS_READ_REQUEST);
+}
+
+void sendTFTPWriteRequest(const char *filename, int sockfd) {
+    // Prepare the RRQ packet
+    char rrqPacket[516]; // Max size for a TFTP packet
+    memset(rrqPacket, 0, sizeof(rrqPacket));
+
+    // Opcode (Write Request)
+    rrqPacket[0] = (TFTP_OPCODE_RRQ_WRITE >> 8) & 0xFF;
+    rrqPacket[1] = TFTP_OPCODE_RRQ_WRITE & 0xFF;
+
+    // Filename
+    strcpy(&rrqPacket[2], filename);
+
+    // Null terminator after the filename
+    int filenameLength = strlen(filename);
+    rrqPacket[2 + filenameLength] = '\0';
+
+    // Mode
+    strcpy(&rrqPacket[2 + filenameLength + 1], TFTP_MODE);
+
+    // Null terminator after the mode
+    int modeLength = strlen(TFTP_MODE);
+    rrqPacket[2 + filenameLength + 1 + modeLength] = '\0';
+
+    //print_raw_byte(rrqPacket, sizeof(rrqPacket));
+
+    // Send the RRQ packet
+    ssize_t bytesSent = send(sockfd, rrqPacket, sizeof(rrqPacket), 0);
+    if (bytesSent == -1) {
+        print_error(ERROR_SENDING_PACKET);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (DEBUG) print_message(MESSAGE_SUCCESS_WRITE_REQUEST);
+}
+
+void sendTFTPData(char * data, int block_number, int sockfd) {
+    // Prepare the RRQ packet
+    char rrqPacket[516]; // Max size for a TFTP packet
+    memset(rrqPacket, 0, sizeof(rrqPacket));
+
+    // Opcode (Data)
+    rrqPacket[0] = (TFTP_OPCODE_DATA >> 8) & 0xFF;
+    rrqPacket[1] = TFTP_OPCODE_DATA & 0xFF;
+
+    // Block Number
+    rrqPacket[2] = (block_number >> 8) & 0xFF;
+    rrqPacket[3] = block_number & 0xFF;
+
+    // Data
+    strcpy(&rrqPacket[4], data);
+
+    // Null terminator after the filename
+    int dataLength = strlen(data);
+    rrqPacket[4 + dataLength] = '\0';
+
+    //print_raw_byte(rrqPacket, sizeof(rrqPacket));
+
+    // Send the RRQ packet
+    ssize_t bytesSent = send(sockfd, rrqPacket, sizeof(rrqPacket), 0);
+    if (bytesSent == -1) {
+        print_error(ERROR_SENDING_PACKET);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (DEBUG) print_message(MESSAGE_SUCCESS_DATA);
+}
+
+void sendTFTPAck(int block_number, int sockfd) {
+    // Prepare the RRQ packet
+    char rrqPacket[516]; // Max size for a TFTP packet
+    memset(rrqPacket, 0, sizeof(rrqPacket));
+
+    // Opcode (ACK)
+    rrqPacket[0] = (TFTP_OPCODE_ACK >> 8) & 0xFF;
+    rrqPacket[1] = TFTP_OPCODE_ACK & 0xFF;
+
+    // Block Number
+    rrqPacket[2] = (block_number >> 8) & 0xFF;
+    rrqPacket[3] = block_number & 0xFF;
+
+    print_raw_byte(rrqPacket, sizeof(rrqPacket));
+
+    // Send the RRQ packet
+    ssize_t bytesSent = send(sockfd, rrqPacket, sizeof(rrqPacket), 0);
+    if (bytesSent == -1) {
+        print_error(ERROR_SENDING_PACKET);
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (DEBUG) print_message(MESSAGE_SUCCESS_ACK);
+}
+
+void receivePacket(int sockfd, char * ip) {
+    // Receive and save the file
+    int blockNumber = 1;
+    print_message("start waiting receive\n");
+    while (1) {
+        char dataPacket[TFTP_BLOCK_SIZE + 4]; // Opcode (2 bytes) + Block Number (2 bytes) + DATA
+
+        // Server details
+        struct sockaddr_in serverAddr;
+        memset(&serverAddr, 0, sizeof(serverAddr));
+        serverAddr.sin_family = AF_INET;
+        serverAddr.sin_port = htons(atoi(PORT)); // TFTP port
+        inet_pton(AF_INET, ip, &(serverAddr.sin_addr));
+
+        ssize_t bytesRead = recvfrom(sockfd, dataPacket, sizeof(dataPacket), 0, (struct sockaddr *) &serverAddr, (socklen_t *) sizeof(serverAddr));
+        if (bytesRead == -1) {
+            perror("Error receiving data packet");
+            close(sockfd);
+            exit(EXIT_FAILURE);
+        }
+
+        print_message("message");
+
+        // Extract Opcode and Block Number
+        int opcode = (dataPacket[0] << 8) | dataPacket[1];
+        int receivedBlockNumber = (dataPacket[2] << 8) | dataPacket[3];
+
+        if (opcode == TFTP_OPCODE_DATA && receivedBlockNumber == blockNumber) {
+            // Write data to the file using write and file descriptor
+            int bytesWritten = write(STDOUT_FILENO, &dataPacket[4], bytesRead - 4);
+
+            if (bytesWritten == -1) {
+                print_error(WRITE_ERROR);
+                close(sockfd);
+                exit(EXIT_FAILURE);
+            }
+
+            // Send ACK for the received block
+            sendTFTPAck(blockNumber, sockfd);
+
+            // Increment block number for the next iteration
+            blockNumber++;
+
+            // Check if the received data packet is less than the maximum size, indicating the last block
+            if (bytesRead < TFTP_BLOCK_SIZE + 4) {
+                break;
+            }
+        } else {
+            // Ignore invalid packets (e.g., duplicates)
+        }
+    }
 }
 
 
@@ -115,12 +310,18 @@ void gettftp(char * host, char * file) {
     file = file; // tmp 
     void * addr = get_ip(host);
     char * str_addr = str_ip(addr);
-    get_socket(str_addr);
-    
-}
+    int sockfd = get_socket(str_addr);
 
+    if (DEBUG) print_message("Connected\n");
+    sendTFTPReadRequest(file, sockfd);
+    receivePacket(sockfd, str_addr);
+
+    close(sockfd); 
+      
+}
+ 
 void puttftp(char * host, char * file) {
     host = host; // tmp
     file = file; // tmp
-    print_message("puttftp test \n");
-}
+    if (DEBUG) print_message("puttftp test \n");
+} 
